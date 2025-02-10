7b275d14663453435697191ae27d566f5f97d5cc
kevingandrades <kevingandrades@gmail.com>
2025-02-10 16:26:54 -0400

    udapte astro to 5.2.5 and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,16 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [ react() ],
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true
-		}
-	}),
-	output: "hybrid",
 	vite: {
 		build: {
 			cssMinify: "lightningcss"
 		},
 		ssr: {
-			noExternal: ["path-to-regexp"]
+			noExternal: [ "path-to-regexp" ]
 		}
 	}
 });
